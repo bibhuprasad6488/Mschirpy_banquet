45d52bbd1181444746b5d59d3521ac56afc8b25a
bibhuprasad6488 <bibhu.prasad6488@gmail.com>
2023-03-13 17:27:18 +0530

    update

--- resources/views/dashboard/tax/create_subcategory.blade.php
+++ resources/views/dashboard/tax/create_subcategory.blade.php
@@ -1,83 +1,85 @@
 @extends('dashboard.mainlayouts')
 @section('title', 'Add Tax Sub Category')
 @section('content')
-<!-- Page Wrapper -->
-<div class="page-wrapper">
-   <div class="content container-fluid">
-      <!-- Page Header -->
-      <div class="page-header">
-         <div class="row">
-            <div class="col-sm-12">
-               <h3 class="page-title">Add Tax Sub Category</h3>
-               <ul class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="/home">Dashboard</a></li>
-                  <li class="breadcrumb-item active">Add Tax Sub Category</li>
-               </ul>
+    <!-- Page Wrapper -->
+    <div class="page-wrapper">
+        <div class="content container-fluid">
+            <!-- Page Header -->
+            <div class="page-header">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h3 class="page-title">Add Tax Sub Category</h3>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="/home">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Add Tax Sub Category</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-      <!-- /Page Header -->
-      <div class="row">
-         <div class="col-sm-12">
-            <div class="card">
-               <div class="card-body">
-                  @if ($message = Session::get('success'))
-                  <div class="alert alert-success alert-block">
-                     <button type="button" class="close" data-dismiss="alert">×</button>
-                     <strong>{{ $message }}</strong>
-                  </div>
-                  @endif
-                  @if ($message = Session::get('error'))
-                  <div class="alert alert-danger alert-block">
-                     <button type="button" class="close" data-dismiss="alert">×</button>
-                     <strong>{{ $message }}</strong>
-                  </div>
-                  @endif
-                  <form action="/store_subcat" method="post" autocomplete="off" enctype="multipart/form-data">
-                     @csrf
-                     <div class="row">
-                        <div class="col-md-6">
-                           <div class="form-group">
-                              <label>Percentage</label>
-                              <span class="text-danger">* (%)</span>
-                              <input type="number" id="subcategory" name="subcategory" class="form-control" placeholder="Sub Category Name" required>
-                           </div>
+            <!-- /Page Header -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                        <div class="card-body">
+                            @if ($message = Session::get('success'))
+                                <div class="alert alert-success alert-block">
+                                    <button type="button" class="close" data-dismiss="alert">×</button>
+                                    <strong>{{ $message }}</strong>
+                                </div>
+                            @endif
+                            @if ($message = Session::get('error'))
+                                <div class="alert alert-danger alert-block">
+                                    <button type="button" class="close" data-dismiss="alert">×</button>
+                                    <strong>{{ $message }}</strong>
+                                </div>
+                            @endif
+                            <form action="/store_subcat" method="post" autocomplete="off" enctype="multipart/form-data">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Category</label>
+                                            <span class="text-danger">*</span>
+                                            <select class="form-control" name="category_id">
+                                                <option value="">Select Category</option>
+                                                @if (!empty($taxcats))
+                                                    @foreach ($taxcats as $k => $cat)
+                                                        <option value="{{ $k }}">{{ $cat }}</option>
+                                                    @endforeach
+                                                @endif
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Percentage</label>
+                                            <span class="text-danger">* (%)</span>
+                                            <input type="number" id="subcategory" name="subcategory" class="form-control"
+                                                placeholder="Percentage" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Status</label>
+                                            <select class="form-control" name="status">
+                                                <option value="active">Active</option>
+                                                <option value="inactive">Inactive</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" id="submit" class="btn btn-primary" name="submit"
+                                        value="Save">
+                                    <input type="reset" class="btn btn-warning" value="Reset">
+                                    <a href="/list_subcat" class="btn btn-secondary mx-1">Back</a>
+                                </div>
+                            </form>
                         </div>
-                        <div class="col-md-6">
-                           <div class="form-group">
-                              <label>Category</label>
-                              <span class="text-danger">*</span>
-                              <select class="form-control" name="category_id">
-                                 <option value="">Select Category</option>
-                                 @if(!empty($taxcats))
-                                 @foreach($taxcats as $k=>$cat)
-                                 <option value="{{$k}}">{{$cat}}</option>
-                                 @endforeach
-                                 @endif
-                              </select>
-                           </div>
-                        </div>
-                        
-                        <div class="col-md-6">
-                           <div class="form-group">
-                              <label>Status</label>
-                              <select class="form-control" name="status">
-                                 <option value="active">Active</option>
-                                 <option value="inactive">Inactive</option>
-                              </select>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="form-group">
-                        <input type="submit" id="submit" class="btn btn-primary" name="submit" value="Save">
-                        <input type="reset" class="btn btn-warning" value="Reset">
-                        <a href="/list_subcat" class="btn btn-secondary mx-1">Back</a>
-                     </div>
-                  </form>
-               </div>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
-</div>
+        </div>
+    </div>
 @endsection
